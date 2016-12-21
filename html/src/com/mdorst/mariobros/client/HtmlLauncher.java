025c510e35392334870dbeb9c8a42c104baa8e20
Michael Dorst <code@mikefdorst.com>
2016-12-20 22:15:30 -0800

    Initial Commit

---
+++ html/src/com/mdorst/mariobros/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mdorst.mariobros.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mdorst.mariobros.MarioBros;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MarioBros();
+        }
+}
