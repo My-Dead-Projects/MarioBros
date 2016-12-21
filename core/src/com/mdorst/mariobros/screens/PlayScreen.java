5d6daa29d0816be6007036da870e9f3f17ed5368
Michael Dorst <code@mikefdorst.com>
2016-12-20 22:35:40 -0800

    Add a viewport

--- core/src/com/mdorst/mariobros/screens/PlayScreen.java
+++ core/src/com/mdorst/mariobros/screens/PlayScreen.java
@@ -3,17 +3,24 @@ package com.mdorst.mariobros.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mdorst.mariobros.MarioBros;
 
 public class PlayScreen implements Screen {
 
     private MarioBros game;
     Texture texture;
+    private OrthographicCamera camera;
+    private Viewport viewport;
 
     public PlayScreen(MarioBros game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(800, 400, camera);
     }
 
     @Override
@@ -25,6 +32,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -32,7 +40,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
     }
 
     @Override
